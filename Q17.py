0ac08f71e7f838e1ae6731abfb4f750f8e9026cd
nix-per <145903046+nix-per@users.noreply.github.com>
2024-06-17 13:07:47 +0530

    Create Q17.py

---
+++ Q17.py
@@ -0,0 +1,4 @@
+# Q17. Write a program in python that converts a given string to title case (first letter of each word capitalized)
+
+x=input("Enter a string: ")
+print("The given string in title case is:", x.title())
