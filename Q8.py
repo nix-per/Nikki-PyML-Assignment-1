961ba778505d4006c01eff645952fa9f099e7443
nix-per <145903046+nix-per@users.noreply.github.com>
2024-06-17 12:44:43 +0530

    Update Q8.py

--- Q8.py
+++ Q8.py
@@ -1,3 +1,5 @@
+# Q8. Write a python program that concatenates two strings and returns the result
+
 x=input("Enter the first string: ")
 y=input("Enter the second string: ")
 print("Concatenated string:", x+y)
