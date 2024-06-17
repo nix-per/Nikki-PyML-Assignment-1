fc580a4911dc788adaf45f271a89e0b92268300c
nix-per <145903046+nix-per@users.noreply.github.com>
2024-06-17 14:09:39 +0530

    Update Q24.py

--- Q24.py
+++ Q24.py
@@ -1,2 +1,20 @@
 # Q24. Write a program that acts as a simple calculator. It should take two numbers and an operator (+, -, *, /) as input and print the result
 
+print("Enter the operators (+, -, *, /) for the desired operation")
+x=float(input("Enter the first number: "))
+y=float(input("Enter the second number: "))
+c=input("Enter the operator of choice: ")
+
+if c=="+" :
+    print(x,"+",y,"=",x+y)
+elif c=="-":
+    print(x,"-",y,"=",x-y)
+elif c=="*":
+    print(x,"*",y,"=",x*y)
+elif c=="/":
+    if y==0:
+        print("Division by zero is undefined")
+    else:
+        print(x,"/",y,"=",x/y)
+else:
+    print("Invalid operator")
