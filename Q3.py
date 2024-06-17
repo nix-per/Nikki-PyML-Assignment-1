8b607ff9d7137de20c46f5c4c359ee3161de3250
nix-per <145903046+nix-per@users.noreply.github.com>
2024-06-17 11:47:07 +0530

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the number: "))
+fac=1
+if(x==0 or x==1):
+    print("Factorial of", x, "is:", fac)
+elif(x<0):
+    print("Factorial of a negative number does not exist")
+else:
+    for i in range (1,x+1):
+        fac*=i
+    print("Factorial of", x, "is:", fac)
