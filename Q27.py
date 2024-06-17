133f95042406ff3ea651dfd6da0379f6c433df11
nix-per <145903046+nix-per@users.noreply.github.com>
2024-06-17 14:30:09 +0530

    Create Q27.py

---
+++ Q27.py
@@ -0,0 +1,5 @@
+# Q27. Write a program in python that converts a string into a list of its characters
+
+x=input("Enter a string: ")
+l1=list(x)
+print("List of characters of the given string is:",l1)
