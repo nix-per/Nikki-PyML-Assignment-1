8a36890c2d26098374c3d886207358a4463a9997
nix-per <145903046+nix-per@users.noreply.github.com>
2024-06-17 12:38:58 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,3 +1,5 @@
+# Q1. Write a program that takes two numbers as input and prints their sum
+
 x=int(input("Enter the first number: "))
 y=int(input("Enter the second number: "))
 print ("The sum of", x , "and", y ,"is:", x+y)
